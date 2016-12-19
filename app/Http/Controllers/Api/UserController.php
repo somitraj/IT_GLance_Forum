84571b4295fbf2c03d06bb2486acac684157c9c4
somitraj <ranjitkarsomit@gmail.com>
2016-12-19 14:57:40 +0545

    intern project

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace IT_Glance_Forum\Http\Controllers\api;
+
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Http\Request;
+use IT_Glance_Forum\Http\Controllers\Controller;
+
+
+class UserController extends Controller
+{
+    public function ApplicationSubmit(Request $request){
+        try{
+
+
+        }
+        catch (\Exception $e) {
+            throw $e;
+        }
+    }
+}
+
