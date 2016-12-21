5f2545940e358a5bea3b58f48e0b0096adcae9d2
Ayush <silent.killer566232@gmail.com>
2016-12-21 14:47:08 +0545

    int

---
+++ app/Http/Middleware/checkSubMentor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace IT_Glance_Forum\Http\Middleware;
+
+use Closure;
+
+class checkSubMentor
+{
+    public function handle($request, Closure $next)
+    {
+
+        if(Auth::check())
+        {
+            $route=Route::getCurrentRoute()->getAction();
+            // print_r(Auth::user()->user_typeid);die();
+            if(array_key_exists('usertype',$route))
+            {
+                if(Auth::user()->user_type_id!=$route['usertype'])
+                {
+                    return redirect()->route('login');
+                }
+            }
+        }
+        return $next($request);
+    }
+}
