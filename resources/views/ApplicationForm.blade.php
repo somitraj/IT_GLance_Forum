817102d0e5eb7f11575193ffbd3b8b5a5bccdbba
somitraj <ranjitkarsomit@gmail.com>
2017-01-04 00:04:13 +0545

    intern project

--- resources/views/ApplicationForm.blade.php
+++ resources/views/ApplicationForm.blade.php
@@ -1,8 +1,9 @@
 @extends('MainLayout')
 @section('contents')
-
-    <div class="jumbotron" >
+    <div class="container-fluid" style="background-color: white">
+    <div class="jumbotron" style="background-color: #667ff5">
         <h2 style="text-align: center"><b>Application Form</b></h2></div>
     {!! form($form) !!}
+    </div>
 
 @endsection
