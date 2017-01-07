08891d5621490a78a31dce95fea88552cac58758
somitraj <ranjitkarsomit@gmail.com>
2017-01-07 21:31:30 +0545

    intern project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,7 @@
 @section('contents')
     <br><br>
     <h1>Welcome</h1>
-  <a href="post" ><button class="btn-lg">Create Your Question</button></a>
+    <a href="post">
+        <button class="btn-lg">Create Your Question</button>
+    </a>
 @endsection
